7a2a8accc6f19f647369217d249b500507814b17
Tim Whitney <timw@slalom.com>
2016-03-01 13:23:50 -0600

    Removed manually configured tomcat instances.

--- support-web/src/main/java/com/whitney/support/SupportApplication.java
+++ support-web/src/main/java/com/whitney/support/SupportApplication.java
@@ -1,11 +1,7 @@
 package com.whitney.support;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.context.embedded.AnnotationConfigEmbeddedWebApplicationContext;
-import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
-import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jms.annotation.EnableJms;
@@ -15,11 +11,5 @@ import org.springframework.jms.annotation.EnableJms;
 @EnableJms
 @ComponentScan(value = { "com.whitney.support", "com.whitney.common" })
 public class SupportApplication extends AnnotationConfigEmbeddedWebApplicationContext {
-    @Value("${support.port:8082}")
-    private Integer port;
 
-    @Bean
-    public EmbeddedServletContainerFactory embeddedServletContainerFactory() {
-        return new TomcatEmbeddedServletContainerFactory(port);
-    }
 }
