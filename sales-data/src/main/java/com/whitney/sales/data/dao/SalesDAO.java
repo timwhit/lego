a76f953916a144e9b2cc25b2fad7d926f5f881f5
Tim Whitney <timw@slalom.com>
2016-03-04 09:40:05 -0600

    Updated version.

--- sales-data/src/main/java/com/whitney/sales/data/dao/SalesDAO.java
+++ sales-data/src/main/java/com/whitney/sales/data/dao/SalesDAO.java
@@ -3,6 +3,4 @@ package com.whitney.sales.data.dao;
 import com.whitney.sales.data.dto.SaleDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface SalesDAO extends JpaRepository<SaleDTO, Long> {
-
-}
+public interface SalesDAO extends JpaRepository<SaleDTO, Long> {}
